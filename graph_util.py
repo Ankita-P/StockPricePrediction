dee24c23049bed20a467c1b2981c5d1dfe5e5883
jayavardhanpatil <jayawardhan.patil@gmail.com>
2020-12-04 11:16:57 -0800

    Knn model and plot_graph def

--- graph_util.py
+++ graph_util.py
@@ -5,21 +5,24 @@ register_matplotlib_converters()
 
 def plot_graph_single_dataset(dataset, title, xlabel, ylabel):
     # Plot the graph of original Prices
+    #plt.ion()
     plt.figure(figsize=(10, 4))
     plt.title(title)
     plt.xlabel(xlabel)
     plt.ylabel(ylabel)
     plt.plot(dataset)
     plt.show()
+    plt.show(block=False)
 
-def plot_graph_original_predicted(dataset, title, xlabel, ylabel):
+def plot_graph_original_predicted(dataset, title, xlabel, ylabel, company_close_price, predictions):
     # Plot the graph of original Prices
-    valid = dataset[x.shape[0]:]
+    valid = dataset
+    valid["Predictions"] = predictions
     plt.figure(figsize=(10, 6))
-    plt.title("Apple's Stock Price Prediction Model(Linear Regression Model)")
-    plt.xlabel("Years")
-    plt.ylabel("Close Price USD ($)")
-    plt.plot(company_close_price["Close"])
+    plt.title(title)
+    plt.xlabel(xlabel)
+    plt.ylabel(ylabel)
+    plt.plot(company_close_price)
     plt.plot(valid[["Close", "Predictions"]])
-    plt.legend(["Original", "Valid", "Predictions"])
+    plt.legend(["Original_train", "Valid_test", "Predictions"])
     plt.show()
--- main.py
+++ main.py
@@ -2,10 +2,12 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LinearRegression
 from sklearn.model_selection import train_test_split
+from sklearn.neighbors import KNeighborsRegressor
+
 import constant_variables
 import graph_util
 
-dataset = pd.read_csv('data/MSFT.csv')
+dataset = pd.read_csv('data/'+constant_variables.STOCK_SYMBL+'.csv')
 dataset['Date'] = pd.to_datetime(dataset.Date,format='%Y-%m-%d')
 dataset.index = dataset['Date']
 
@@ -20,22 +22,29 @@ company_close_price = dataset[["Close"]]
 company_close_price["Prediction"] = company_close_price[["Close"]].shift(-constant_variables.PREDICTION_DAYS)
 
 x = np.array(company_close_price.drop(["Prediction"], 1))[:-constant_variables.PREDICTION_DAYS]
-
 y = np.array(company_close_price["Prediction"])[:-constant_variables.PREDICTION_DAYS]
 
 #25% of the data is test Data
 x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=constant_variables.TEST_DATA_SIZE)
 
-linear = LinearRegression().fit(x_train, y_train)
 
 future_prices = company_close_price.drop(["Prediction"], 1)[:-constant_variables.PREDICTION_DAYS]
 future_prices = future_prices.tail(constant_variables.PREDICTION_DAYS)
 future_prices = np.array(future_prices)
 print(future_prices)
 
+#Linear regression
+linear = LinearRegression().fit(x_train, y_train)
 linear_prediction = linear.predict(future_prices)
 print("Linear regression Prediction =",linear_prediction)
+graph_util.plot_graph_original_predicted(dataset[x.shape[0]:], "Apple's Stock Price Prediction Model - [Linear Regression Model]",
+                                         "Years", "Close Price", company_close_price["Close"], linear_prediction)
 
 
-
+#K - nearest neighbour Confidence
+knn = KNeighborsRegressor().fit(x_train, y_train)
+knn_prediction = knn.predict(future_prices)
+print("K-nearest neighbour Prediction =",knn_prediction)
+graph_util.plot_graph_original_predicted(dataset[x.shape[0]:], "Apple's Stock Price Prediction Model - [Knn Model]",
+                                         "Years", "Close Price", company_close_price["Close"], knn_prediction)
 
