da8e5a4e556a6bdcd9c97305369d37e5bcef5cd1
jayavardhanpatil <jayawardhan.patil@gmail.com>
2020-12-05 15:30:27 -0800

    LSTM code integrated

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ import numpy as np
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.neighbors import KNeighborsRegressor
+from sklearn.preprocessing import MinMaxScaler
+from keras.models import Sequential
+from keras.layers import Dense, LSTM
 
 import constant_variables
 import graph_util
@@ -11,16 +14,13 @@ dataset = pd.read_csv('data/'+constant_variables.STOCK_SYMBL+'.csv')
 dataset['Date'] = pd.to_datetime(dataset.Date,format='%Y-%m-%d')
 dataset.index = dataset['Date']
 
-graph_util.plot_graph_single_dataset(dataset["Close"], "Microsoft's Stock Price", "Year", "Close Price USD ($)")
-
 #Filter data from date:
 dataset = dataset[(dataset['Date'] > constant_variables.START_DATE) & (dataset['Date'] < constant_variables.END_DATE)]
 
+graph_util.plot_graph_single_dataset(dataset["Close"], "Microsoft's Stock Price", "Year", "Close Price USD ($)")
+
 #Consider Close prices for training
 company_close_price = dataset[["Close"]]
-
-test_data_size = int(len(dataset) * constant_variables.TEST_DATA_SIZE)
-train_data_size = len(dataset) - test_data_size;
 
 company_close_price["Prediction"] = company_close_price[["Close"]]
 
@@ -28,19 +28,23 @@ company_close_price["Prediction"] = company_close_price[["Close"]]
 #x = np.array(company_close_price.drop(["Prediction"], 1))[:-constant_variables.PREDICTION_DAYS]
 #y = np.array(company_close_price["Prediction"])[:-constant_variables.PREDICTION_DAYS]
 
-x = company_close_price.drop(["Prediction"],1)
-y = company_close_price["Prediction"]
+# x = company_close_price.drop(["Prediction"],1)
+# y = company_close_price["Prediction"]
 
-#25% of the data is test Data
-x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=constant_variables.TEST_DATA_SIZE)
+#% of the data is test Data
+# x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=constant_variables.TEST_DATA_SIZE)
 
-#x = company_close_price[:train_data_size]
-#y = company_close_price[train_data_size:]
+test_data_size = int(len(dataset) * constant_variables.TEST_DATA_SIZE)
+train_data_size = len(dataset) - test_data_size;
 
-#x_train = x.drop('Close', axis=1)
-#y_train = x['Close']
-#x_test = y.drop('Close', axis=1)
-#y_test = y['Close']
+#Split on sequence of train and test data.
+x = company_close_price[:train_data_size]
+y = company_close_price[train_data_size:]
+
+x_train = x.drop('Close', axis=1)
+y_train = x['Close']
+x_test = y.drop('Close', axis=1)
+y_test = y['Close']
 
 future_prices = company_close_price.drop(["Prediction"], 1)[:-len(x_test)]
 future_prices = future_prices.tail(len(x_test))
@@ -62,3 +66,50 @@ print("K-nearest neighbour Prediction =",knn_prediction)
 graph_util.plot_graph_original_predicted(dataset[-len(x_test):], "Apple's Stock Price Prediction Model - [Knn Model]",
                                          "Years", "Close Price", company_close_price["Close"], knn_prediction)
 
+
+#LSTM
+scaler = MinMaxScaler(feature_range=(0, 1))
+data = dataset.filter(["Close"])
+data.reset_index()
+scaled_data = scaler.fit_transform(data)
+
+training_data_len = len(x_train)
+
+train_data = scaled_data[0:training_data_len]
+x_train=[]
+y_train = []
+for i in range(60, len(train_data)):
+    x_train.append(train_data[i-60:i, 0])
+    y_train.append(train_data[i, 0])
+
+x_train, y_train = np.array(x_train), np.array(y_train)
+x_train = np.reshape(x_train, (x_train.shape[0],x_train.shape[1],1))
+
+model = Sequential()
+model.add(LSTM(units=50, return_sequences=True,input_shape=(x_train.shape[1],1)))
+model.add(LSTM(units=50, return_sequences=False))
+model.add(Dense(units=25))
+model.add(Dense(units=1))
+
+model.compile(optimizer='adam', loss='mean_squared_error')
+
+#Train the model on x and y datasets
+model.fit(x_train, y_train, batch_size=1, epochs=1)
+
+#Test data set
+test_data = scaled_data[training_data_len - 60:]
+
+x_test = []
+y_test =  dataset[training_data_len:]
+for i in range(60,len(test_data)):
+    x_test.append(test_data[i-60:i,0])
+
+x_test = np.array(x_test)
+x_test = np.reshape(x_test, (x_test.shape[0],x_test.shape[1],1))
+
+# Predicting values using LSTM
+predictions = model.predict(x_test)
+predictions = scaler.inverse_transform(predictions)#Undo scaling
+
+graph_util.plot_graph_original_predicted(dataset[-len(x_test):], "Apple's Stock Price Prediction Model - [LSTM]",
+                                         "Years", "Close Price", company_close_price["Close"], predictions)
