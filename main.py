da348fb080d37da1339be9270c7c6d4b48bb28ec
jayavardhanpatil <jayawardhan.patil@gmail.com>
2020-12-10 00:53:43 -0800

    Added other output data

--- main.py
+++ main.py
@@ -7,10 +7,12 @@ from sklearn.preprocessing import MinMaxScaler
 from keras.models import Sequential
 from keras.layers import Dense, LSTM
 from statsmodels.tsa.arima_model import ARIMA
+from sklearn import metrics
 
 import constant_variables
 import graph_util
 
+#Read the stocks input data
 dataset = pd.read_csv('data/'+constant_variables.STOCK_SYMBL+'.csv')
 dataset['Date'] = pd.to_datetime(dataset.Date,format='%Y-%m-%d')
 dataset.index = dataset['Date']
@@ -18,11 +20,15 @@ dataset.index = dataset['Date']
 #Filter data from date:
 dataset = dataset[(dataset['Date'] > constant_variables.START_DATE) & (dataset['Date'] < constant_variables.END_DATE)]
 
+#plot original graph
 graph_util.plot_graph_single_dataset(dataset["Close"], constant_variables.STOCK_SYMBL+"'s Stock Price", "Year", "Close Price USD ($)")
 
 #Consider Close prices for training
 company_close_price = dataset[["Close"]]
 
+#copy some sample data to Prediction column
+#this is just a dummy price later we are dropping it while predicting the
+#this value in prediction could be any value
 company_close_price["Prediction"] = company_close_price[["Close"]]
 
 #company_close_price["Prediction"] = company_close_price[["Close"]].shift(-test_data_size)
@@ -35,6 +41,8 @@ company_close_price["Prediction"] = company_close_price[["Close"]]
 #% of the data is test Data
 # x_train, x_test, y_train, y_test = train_test_split(x, y, test_size=constant_variables.TEST_DATA_SIZE)
 
+#Identify the length of the train and test data
+#read the test size from config file
 test_data_size = int(len(dataset) * constant_variables.TEST_DATA_SIZE)
 train_data_size = len(dataset) - test_data_size;
 
@@ -42,35 +50,35 @@ train_data_size = len(dataset) - test_data_size;
 x = company_close_price[:train_data_size]
 y = company_close_price[train_data_size:]
 
+#Assign Train and test data, and drop the close prices from x_train and x_test for prediciton
 x_train = x.drop('Close', axis=1)
 y_train = x['Close']
 x_test = y.drop('Close', axis=1)
 y_test = y['Close']
 
+#get the future days value on which we need to predict
 future_prices = company_close_price.drop(["Prediction"], 1)[:-len(x_test)]
 future_prices = future_prices.tail(len(x_test))
 future_prices = np.array(future_prices)
 print(future_prices)
 
 #Linear regression
+#predict the price
 linear = LinearRegression().fit(x_train, y_train)
 linear_prediction = linear.predict(future_prices)
 
-rms=np.sqrt(np.mean(np.power((np.array(y_test)-np.array(linear_prediction)),2)))
-print("Linear Regression RMSE : ",rms)
-
+#Plot the original, actual and predicted values on graph
 print("Linear regression Prediction =",linear_prediction)
 graph_util.plot_graph_original_predicted(dataset[-len(x_test):], constant_variables.STOCK_SYMBL+"'s Stock Price Prediction Model - [Linear Regression Model]",
                                          "Years", "Close Price", company_close_price["Close"], linear_prediction)
 
 
 #K - nearest neighbour Confidence
+#Predict using knn model
 knn = KNeighborsRegressor().fit(x_train, y_train)
 knn_prediction = knn.predict(future_prices)
 
-rms=np.sqrt(np.mean(np.power((np.array(y_test)-np.array(knn_prediction)),2)))
-print("K-nearest neighbour RMSE : ",rms)
-
+#plot the original, actual, and predicted values on graph
 print("K-nearest neighbour Prediction =",knn_prediction)
 graph_util.plot_graph_original_predicted(dataset[-len(x_test):], constant_variables.STOCK_SYMBL+"'s Stock Price Prediction Model - [Knn Model]",
                                          "Years", "Close Price", company_close_price["Close"], knn_prediction)
@@ -117,14 +125,11 @@ x_test = np.array(x_test)
 x_test = np.reshape(x_test, (x_test.shape[0],x_test.shape[1],1))
 
 # Predicting values using LSTM
-predictions = model.predict(x_test)
-predictions = scaler.inverse_transform(predictions)#Undo scaling
+lstm_predictions = model.predict(x_test)
+lstm_predictions = scaler.inverse_transform(lstm_predictions)#Undo scaling
 
-rms=np.sqrt(np.mean(np.power((np.array(y_test['Close'])-np.array(predictions)),2)))
-print("LSTM RMSE : ",rms)
-
-graph_util.plot_graph_original_predicted(dataset[-len(x_test):], constant_variables.STOCK_SYMBL+"'s Stock Price Prediction Model - [LSTM]",
-                                         "Years", "Close Price", company_close_price["Close"], predictions)
+graph_util.plot_graph_original_predicted(dataset[-len(x_test):], constant_variables.STOCK_SYMBL +"'s Stock Price Prediction Model - [LSTM]",
+                                         "Years", "Close Price", company_close_price["Close"], lstm_predictions)
 
 
 #ARIMA,
@@ -148,10 +153,21 @@ for time_point in range(N_test_observations):
     count += 1
     print("........",count)
 
-
-rms=np.sqrt(np.mean(np.power((np.array(testing_data)-np.array(model_predictions)),2)))
-print("ARIMA RMSE : ",rms)
-
-
 graph_util.plot_graph_original_predicted(dataset[-len(testing_data):], constant_variables.STOCK_SYMBL+"'s Stock Price Prediction Model - [ARIMA]",
                                          "Years", "Close Price", company_close_price["Close"], model_predictions)
+
+output_val = pd.DataFrame(company_close_price[train_data_size:])
+output_val.drop(["Prediction"], axis=1, inplace=True)
+output_val["LINEAR_REGRESSION"] = linear_prediction
+output_val["KNN"] = knn_prediction
+output_val["LSTM"] = lstm_predictions
+output_val["ARIMA"] = model_predictions
+
+print(constant_variables.STOCK_SYMBL)
+
+print("Linear regression rmse : ", metrics.mean_squared_error(company_close_price['Close'][train_data_size:], linear_prediction, squared=False))
+print("KNN rmse : ",metrics.mean_squared_error(company_close_price['Close'][train_data_size:], knn_prediction, squared=False))
+print("LSTM rmse : ",metrics.mean_squared_error(company_close_price['Close'][train_data_size:], lstm_predictions, squared=False))
+print("ARIMA rmse : ",metrics.mean_squared_error(company_close_price['Close'][train_data_size:], model_predictions, squared=False))
+
+output_val.to_csv("data/"+constant_variables.STOCK_SYMBL+"_output.csv")
